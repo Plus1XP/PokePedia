ca900f2398a192612617cf024b6cda4043c00073
Plus1XP <evldev@protonmail.ch>
2020-12-16 11:18:08 +0000

    Feat(DataModel): Add MicroCharts Class to Models

---
+++ PokeDex/PokeDex/Models/PokemonCharts.cs
@@ -0,0 +1,30 @@
+﻿using Microcharts;
+
+using SkiaSharp;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeDex.Models
+{
+    class PokemonCharts
+    {
+        public List<Microcharts.ChartEntry> chartEntries;
+
+        public List<Microcharts.ChartEntry>GetChartEntries()
+        {
+            return chartEntries;
+        }
+
+        public Microcharts.ChartEntry AddChartEntries(string name, int? value, string colour)
+        {
+            return new Microcharts.ChartEntry((float)value)
+            {
+                Label = name,
+                ValueLabel = value.ToString(),
+                Color = SKColor.Parse(colour)
+            };
+        }
+    }
+}
