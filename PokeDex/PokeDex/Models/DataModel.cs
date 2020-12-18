4c57a87eb2ad7ec1831c8cf9f8c08488e25dc124
Plus1XP <evldev@protonmail.ch>
2020-12-18 19:37:01 +0000

    Feat(DataModel): Add DataModel to Serialize/Deserialize Pokemon List

---
+++ PokeDex/PokeDex/Models/DataModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace PokeDex.Models
+{
+    class DataModel
+    {
+        public bool ConfirmFileExists(string file)
+        {
+            return File.Exists(file);
+        }
+
+        public bool VerfiyFileContents(string file, object localData)
+        {
+            return LoadDataFromFile(file).Equals(localData);
+        }
+
+        public Object LoadDataFromFile(string file)
+        {
+            return JsonConvert.DeserializeObject<List<PokedexModel>>(File.ReadAllText(file));
+        }
+
+        public void SaveDataToFile(string file, Object data)
+        {
+            File.WriteAllText(file, JsonConvert.SerializeObject(data));
+        }
+    }
+}
