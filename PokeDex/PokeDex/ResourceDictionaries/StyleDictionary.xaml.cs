b0aee2c05dd4d207404e99d27589b8a18fd57e13
Plus1XP <evldev@protonmail.ch>
2020-12-23 14:48:28 +0000

    Feat(UI): Add ResourceDictionary to project

---
+++ PokeDex/PokeDex/ResourceDictionaries/StyleDictionary.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PokeDex.ResourceDictionaries
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StyleDictionary : ResourceDictionary
+    {
+        public StyleDictionary()
+        {
+            InitializeComponent();
+        }
+    }
+}
