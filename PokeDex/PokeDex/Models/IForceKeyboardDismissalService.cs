f6184186f217e073113a17442f276566f83a16fc
Plus1XP <evldev@protonmail.ch>
2020-12-23 19:29:22 +0000

    Feat(DataModel): Add Interface to natively hide IOS keyboard

---
+++ PokeDex/PokeDex/Models/IForceKeyboardDismissalService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeDex.Models
+{
+    public interface IForceKeyboardDismissalService
+    {
+        void DismissKeyboard();
+    }
+}
