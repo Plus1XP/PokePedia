50329845e4393166c6ba7768220eccc4978af765
Plus1XP <evldev@protonmail.ch>
2021-01-08 10:16:12 +0000

    Feat(FileManager): Add XML Serializer to DataManager

--- PokeDex/PokeDex/Models/DataModel.cs
+++ PokeDex/PokeDex/Models/DataModel.cs
@@ -2,6 +2,7 @@
 
 using System.Collections.Generic;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace PokeDex.Models
 {
@@ -14,18 +15,36 @@ namespace PokeDex.Models
 
         public bool VerfiyFileContents(string file, object localData)
         {
-            return LoadDataFromFile(file).Equals(localData);
+            return LoadDataFromXML(file).Equals(localData);
         }
 
-        public object LoadDataFromFile(string file)
+        public object LoadDataFromXML(string file)
         {
-            return JsonConvert.DeserializeObject<List<PokedexModel>>(File.ReadAllText(file));
+            XmlSerializer xml = new XmlSerializer(typeof(List<PokedexModel>));
+            using (StreamReader reader = new StreamReader(file))
+            {
+                return xml.Deserialize(reader);
+            }
         }
 
-        public void SaveDataToFile(string file, object data)
+        public void SaveDataToXML(string file, object data)
         {
-            File.WriteAllText(file, JsonConvert.SerializeObject(data));
+            XmlSerializer xml = new XmlSerializer(typeof(List<PokedexModel>));
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                xml.Serialize(writer, data);
+            }
         }
+
+        //public object LoadDataFromFile(string file)
+        //{
+        //    return JsonConvert.DeserializeObject<List<PokedexModel>>(File.ReadAllText(file));
+        //}
+
+        //public void SaveDataToFile(string file, object data)
+        //{
+        //    File.WriteAllText(file, JsonConvert.SerializeObject(data));
+        //}
 
         public void DeleteDataFile(string file)
         {
